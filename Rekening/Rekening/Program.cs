65608ea56de6548bd48aa58b2018a6fe35b85a51
tom.hollestelle <tom.hollestelle@immalle.be>
2017-05-11 11:54:27 +0200

    bankrekening

---
+++ Rekening/Rekening/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bankrekening b1 = new Bankrekening();
+            
+            bool menu = false;
+
+            while (!menu)
+            {
+
+            Console.WriteLine("1. Geld storten op de bankrekening.");
+            Console.WriteLine("2. Geld afhalen van de bankrekening.");
+            Console.WriteLine("3. Saldo bekijken.");
+            Console.WriteLine("4. Exit.");
+            int keuze = Convert.ToInt16(Console.ReadLine());
+                switch (keuze)
+                {
+                    case 1:
+                        Console.WriteLine("Hoeveel geld wilt u storten?");
+                        int hoeveel = Convert.ToInt32(Console.ReadLine());
+                        b1.saldo += hoeveel;                
+                        break;
+                    case 2:
+                        Console.WriteLine("Hoeveel geld wilt u afhalen?");
+                        int hoeveel2 = Convert.ToInt32(Console.ReadLine());
+                        if (hoeveel2 > b1.saldo)
+                        {
+                            Console.WriteLine("Saldo ontoereikbaar.");
+                        }
+                        else
+                        {
+                            b1.saldo -= hoeveel2;  
+                        }
+                                    
+                        break;
+                    case 3:
+                        Console.WriteLine("Uw saldo bedraagt " + Convert.ToString(b1.saldo));
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                }
+                
+            }
+        }
+
+
+
+    }
+}
---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        Bankrekening b1 = new Bankrekening();
+        
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            int aGeld = Convert.ToInt16(addText.Text);
+            
+        }
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
